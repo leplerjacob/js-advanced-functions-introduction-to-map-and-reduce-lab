714455a452ca1841c96e1fbf17b6c0944c63fcc3
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-22 12:47:48 -0500

    commit tree d547e4cdc52ef96c288ef48fcaf7bff98f52c35e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    const newArray = []
+    sourceArray.forEach(num => {
+        newArray.push(num * -1)
+    })
+    return newArray
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray
+}
+
+function mapToDouble(sourceArray){
+    sourceArray.map()
+}
